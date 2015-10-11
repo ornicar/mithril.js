680477a381aad50adf7bc18335431c240f77d8bc
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-10-12 01:51:42 +0200

    remove build try-catch to allow V8 optimization
    may break I.E. <= 11

--- mithril.js
+++ mithril.js
@@ -555,40 +555,34 @@ var m = (function app(window, undefined) {
 			var cachedAttr = cachedAttrs[attrName];
 			if (!(attrName in cachedAttrs) || (cachedAttr !== dataAttr)) {
 				cachedAttrs[attrName] = dataAttr;
-				try {
-					//`config` isn't a real attributes, so ignore it
-					if (attrName === "config" || attrName === "key") continue;
-					//hook event handlers to the auto-redrawing system
-					else if (isFunction(dataAttr) && attrName.slice(0, 2) === "on") {
-						node[attrName] = autoredraw(dataAttr, node);
-					}
-					//handle `style: {...}`
-					else if (attrName === "style" && dataAttr != null && isObject(dataAttr)) {
-						for (var rule in dataAttr) {
-							if (cachedAttr == null || cachedAttr[rule] !== dataAttr[rule]) node.style[rule] = dataAttr[rule];
-						}
-						for (var rule in cachedAttr) {
-							if (!(rule in dataAttr)) node.style[rule] = "";
-						}
-					}
-					//handle SVG
-					else if (namespace != null) {
-						if (attrName === "href") node.setAttributeNS("http://www.w3.org/1999/xlink", "href", dataAttr);
-						else node.setAttribute(attrName === "className" ? "class" : attrName, dataAttr);
-					}
-					//handle cases that are properties (but ignore cases where we should use setAttribute instead)
-					//- list and form are typically used as strings, but are DOM element references in js
-					//- when using CSS selectors (e.g. `m("[style='']")`), style is used as a string, but it's an object in js
-					else if (attrName in node && attrName !== "list" && attrName !== "style" && attrName !== "form" && attrName !== "type" && attrName !== "width" && attrName !== "height") {
-						//#348 don't set the value if not needed otherwise cursor placement breaks in Chrome
-						if (tag !== "input" || node[attrName] !== dataAttr) node[attrName] = dataAttr;
-					}
-					else node.setAttribute(attrName, dataAttr);
+				//`config` isn't a real attributes, so ignore it
+				if (attrName === "config" || attrName === "key") continue;
+				//hook event handlers to the auto-redrawing system
+				else if (isFunction(dataAttr) && attrName.slice(0, 2) === "on") {
+				node[attrName] = autoredraw(dataAttr, node);
 				}
-				catch (e) {
-					//swallow IE's invalid argument errors to mimic HTML's fallback-to-doing-nothing-on-invalid-attributes behavior
-					if (e.message.indexOf("Invalid argument") < 0) throw e;
+				//handle `style: {...}`
+				else if (attrName === "style" && dataAttr != null && isObject(dataAttr)) {
+				for (var rule in dataAttr) {
+						if (cachedAttr == null || cachedAttr[rule] !== dataAttr[rule]) node.style[rule] = dataAttr[rule];
 				}
+				for (var rule in cachedAttr) {
+						if (!(rule in dataAttr)) node.style[rule] = "";
+				}
+				}
+				//handle SVG
+				else if (namespace != null) {
+				if (attrName === "href") node.setAttributeNS("http://www.w3.org/1999/xlink", "href", dataAttr);
+				else node.setAttribute(attrName === "className" ? "class" : attrName, dataAttr);
+				}
+				//handle cases that are properties (but ignore cases where we should use setAttribute instead)
+				//- list and form are typically used as strings, but are DOM element references in js
+				//- when using CSS selectors (e.g. `m("[style='']")`), style is used as a string, but it's an object in js
+				else if (attrName in node && attrName !== "list" && attrName !== "style" && attrName !== "form" && attrName !== "type" && attrName !== "width" && attrName !== "height") {
+				//#348 don't set the value if not needed otherwise cursor placement breaks in Chrome
+				if (tag !== "input" || node[attrName] !== dataAttr) node[attrName] = dataAttr;
+				}
+				else node.setAttribute(attrName, dataAttr);
 			}
 			//#348 dataAttr may not be a string, so use loose comparison (double equal) instead of strict (triple equal)
 			else if (attrName === "value" && tag === "input" && node.value != dataAttr) {
